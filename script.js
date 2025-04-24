dafaf5d0adeeee6d45d4ae9a8674393efa34c523
ricwi1 <lunichgt@gmail.com>
2025-04-24 21:28:34 +0900

    scrip add

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
+const mainBlock = document.querySelector("main");
 const button = document.querySelector(".button_expand__text");
+const brends = document.querySelector(".for1120");
 
 button.addEventListener("click", function (){
     button.textContent = button.textContent === "Показать все" ? "Скрыть" : "Показать все";
-    
+    if(button.textContent === "Показать все" ){
+        brends.style.display = "none";
+        mainBlock.style.height = "250px";
+    }else{
+        brends.style.display = "flex";
+        mainBlock.style.height = "350px";
+    }
 })
 
 console.log("hi");
